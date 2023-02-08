3e061012037dd533c11bea1867cd90b672792549
Chette <80092129+MrChette@users.noreply.github.com>
2023-02-08 10:26:12 +0100

    SEEDER

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(\App\Cicles::class, 100)->create();
+        factory(\App\Articles::class, 100)->create();
+        factory(\App\User::class)->create(['name' => 'Administrador', 'email' => 'admin@admin.com', 'password' => bcrypt('12345678'), 'type' => 'a', 'actived' => 1, 'email_verified_at' => "1999-01-01 00:00:00"]);
+        factory(\App\User::class, 100)->create(['type'=>'u']);
+        factory(\App\Offers::class, 100)->create();
+        factory(\App\Requirements::class, 100)->create();
+        factory(\App\Applied::class, 100)->create(); // $this->call(UsersTableSeeder::class);
     }
 }
