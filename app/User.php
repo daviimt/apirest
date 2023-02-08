bd3333f903eafeeec3eab2189d4301a9bb22b5e0
Chette <80092129+MrChette@users.noreply.github.com>
2023-02-08 10:50:28 +0100

    z<x

--- app/User.php
+++ app/User.php
@@ -2,66 +2,39 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use App\Mail\UserVerificationEmail;
-use Illuminate\Support\Facades\Mail;
+use Laravel\Passport\HasApiTokens;
 
-
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens,Notifiable;
 
-    public function sendEmailVerificationNotification()
-    {
-       $this->notify(new Notifications\UserVerificationEmail);
-    }
-   
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'name', 'surname', 'email', 'password','type', 'cicle_id',
+        'name', 'email', 'password',
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function appliedsOffers(){
-        return $this->hasMany(Applied::class);
-    }
-
 }
-
-// class User extends Authenticatable
-// {
-//     use Notifiable;
-
-//     /**
-//      * The attributes that are mass assignable.
-//      *
-//      * @var array
-//      */
-//     protected $fillable = [
-//         'name', 'surname', 'email', 'password', 'cicle_id',
-//     ];
-
-//     /**
-//      * The attributes that should be hidden for arrays.
-//      *
-//      * @var array
-//      */
-//     protected $hidden = [
-//         'password', 'remember_token',
-//     ];
-
-//     /**
-//      * The attributes that should be cast to native types.
-//      *
-//      * @var array
-//      */
-//     protected $casts = [
-//         'email_verified_at' => 'datetime',
-//     ];
-// }
